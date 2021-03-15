14d54c5f14f7494ca065233d44b3c951d85cb99b
カグチ <59391215+KagChi@users.noreply.github.com>
2021-03-15 17:37:41 +0700

    chore(ReadListener): add bot presence

--- src/listeners/ReadyListener.ts
+++ src/listeners/ReadyListener.ts
@@ -4,5 +4,6 @@ export default class ReadyListener extends Listener {
     public name = 'ready'
     public exec() {
         logger.info(`${this.client.user.username.toUpperCase()} READY `)
+        this.client.editStatus("dnd", { name: `${this.client.guilds.size} Guilds Using ${this.client.user.username}`, type: 2 })
     }
 }
