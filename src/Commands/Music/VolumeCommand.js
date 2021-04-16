69dcf15937309a36cef01cf0d1254401bd632198
カグチ <59391215+KagChi@users.noreply.github.com>
2021-04-16 18:30:45 +0700

    fix: typo

--- src/Commands/Music/VolumeCommand.js
+++ src/Commands/Music/VolumeCommand.js
@@ -36,7 +36,7 @@ module.exports = class VolumeCommand extends Command {
       const GuildPlayers = this.client.erela.players.get(msg.guild.id);
       if (!GuildPlayers) return msg.channel.send(CreateEmbed('info', '⛔ | There no music playing in this guild'));
       if (!msg.member.voice.channelID) return msg.channel.send(CreateEmbed('warn', '⛔ | you must join voice channel to do this.'));
-      if (msg.member.voice.channelID !== GuildPlayers.VoiceChannel) return msg.channel.send(CreateEmbed('warn', '⛔ | you must join voice channel same as me to do this.'));
+      if (msg.member.voice.channelID !== GuildPlayers.voiceChannel) return msg.channel.send(CreateEmbed('warn', '⛔ | you must join voice channel same as me to do this.'));
       GuildPlayers.setVolume(volume);
       return msg.channel.send(CreateEmbed('info', `👌 | Set guild volume to \`${volume}\``));
     } catch (e) {
