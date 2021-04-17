40e657d2ab95e74bb82adbbac200efe8aa0d0d06
カグチ <59391215+KagChi@users.noreply.github.com>
2021-04-17 15:40:40 +0700

    fix: when someone disconnected bot throw player destroy

--- src/Listeners/playerMove.js
+++ src/Listeners/playerMove.js
@@ -9,8 +9,9 @@ module.exports = class trackStart extends Listener {
   }
 
   async exec(player, oldChannel, newChannel) {
-    this.client.logger.info(`PLAYER MOVED TO [${newChannel}]`);
+    this.client.logger.info(newChannel ? `PLAYER MOVED TO [${newChannel}]` : 'SOMEONE DISCONNECTED ME FROM VOICECHANNEL');
     /* eslint no-param-reassign: "off" */
+    if(!newChannel) return player.destroy();
     player.voiceChannel = newChannel;
     setTimeout(() => player.pause(false), 3000);
   }
