e4fd09dc3ea80be389cf0ccbe01b5a96d34081ff
iMu7aMMaD <69051329+iMu7aMMaD@users.noreply.github.com>
2021-05-21 15:51:51 +0200

    Update VolumeCommand.js

--- src/Commands/Music/VolumeCommand.js
+++ src/Commands/Music/VolumeCommand.js
@@ -34,14 +34,14 @@ module.exports = class VolumeCommand extends Command {
   async exec(msg, { volume }) {
     try {
       const GuildPlayers = this.client.erela.players.get(msg.guild.id);
-      if (!GuildPlayers) return msg.channel.send(CreateEmbed('info', '⛔ | There no music playing in this guild'));
-      if (!msg.member.voice.channelID) return msg.channel.send(CreateEmbed('warn', '⛔ | you must join voice channel to do this.'));
-      if (msg.member.voice.channelID !== GuildPlayers.voiceChannel) return msg.channel.send(CreateEmbed('warn', '⛔ | you must join voice channel same as me to do this.'));
+      if (!GuildPlayers) return msg.channel.send(CreateEmbed('info', '⛔ | There no music playing in this guild.'));
+      if (!msg.member.voice.channelID) return msg.channel.send(CreateEmbed('warn', '⛔ | You must join voice channel to do this.'));
+      if (msg.member.voice.channelID !== GuildPlayers.voiceChannel) return msg.channel.send(CreateEmbed('warn', '⛔ | You must join voice channel same as me to do this.'));
       GuildPlayers.setVolume(volume);
-      return msg.channel.send(CreateEmbed('info', `👌 | Set guild volume to \`${volume}\``));
+      return msg.channel.send(CreateEmbed('info', `👌 | Set guild volume to \`${volume}\`.`));
     } catch (e) {
       this.client.logger.error(e.message);
-      return msg.channel.send(CreateEmbed('warn', '⛔ | An error occured'));
+      return msg.channel.send(CreateEmbed('warn', '⛔ | An error occured.'));
     }
   }
 };
