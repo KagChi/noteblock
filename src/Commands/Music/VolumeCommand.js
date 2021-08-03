673f790596b3e3c828fe4feaf8771e8bcd0fb200
カグチ <59391215+KagChi@users.noreply.github.com>
2021-08-03 17:04:46 +0700

    chore: djs v13 compability

--- src/Commands/Music/VolumeCommand.js
+++ src/Commands/Music/VolumeCommand.js
@@ -28,8 +28,8 @@ module.exports = class VolumeCommand extends Command {
     try {
       const GuildPlayers = this.client.erela.players.get(msg.guild.id);
       if (!GuildPlayers) return msg.channel.send({ embeds: [CreateEmbed('info', '⛔ | There no music playing in this guild')] });
-      if (!msg.member.voice.channelId) return msg.channel.send(CreateEmbed('warn', '⛔ | you must join voice channel to do this.'));
-      if (msg.member.voice.channelId !== GuildPlayers.voiceChannel) return msg.channel.send(CreateEmbed('warn', '⛔ | you must join voice channel same as me to do this.'));
+      if (!msg.member.voice.channelId) return msg.channel.send({ embeds: [CreateEmbed('warn', '⛔ | you must join voice channel to do this.')] });
+      if (msg.member.voice.channelId !== GuildPlayers.voiceChannel) return msg.channel.send({ embeds: [CreateEmbed('warn', '⛔ | you must join voice channel same as me to do this.')] });
       GuildPlayers.setVolume(volume);
       return msg.channel.send({ embeds: [CreateEmbed('info', `👌 | Set guild volume to \`${volume}\``)] });
     } catch (e) {
