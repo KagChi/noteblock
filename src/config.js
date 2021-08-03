9a01fddbea2c2549a98d70718d61c61aaa6421f2
カグチ <59391215+KagChi@users.noreply.github.com>
2021-08-03 17:02:38 +0700

    chore: remove broken lavalink node

--- src/config.js
+++ src/config.js
@@ -1,11 +1,6 @@
 module.exports = {
   owners: [],
   nodes: [{
-    identifier: 'NODE-MAIN',
-    host: 'lavalink-wibu.menggokil.tk',
-    port: 443,
-    secure: true,
-  }, {
     identifier: 'NODE-MAIN-2',
     host: 'lavalink-repl.ahmasa.repl.co',
     port: 443,
