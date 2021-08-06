2c8045e352941126a976ea11e42886366a345d43
KChi <59391215+KagChi@users.noreply.github.com>
2021-08-06 15:48:25 +0000

    fix: channelID > channelId

--- src/Commands/Music/LoopCommand.js
+++ src/Commands/Music/LoopCommand.js
@@ -17,8 +17,8 @@ module.exports = class LoopCommand extends Command {
     try {
       const GuildPlayers = this.client.erela.players.get(msg.guild.id);
       if (!GuildPlayers) return msg.channel.send({ embeds: [CreateEmbed('info', '⛔ | There no music playing in this guild')] });
-      if (!msg.member.voice.channelID) return msg.channel.send({ embeds: [CreateEmbed('warn', '⛔ | you must join voice channel to do this.')] });
-      if (msg.member.voice.channelID !== GuildPlayers.voiceChannel) return msg.channel.send({ embeds: [CreateEmbed('warn', '⛔ | you must join voice channel same as me to do this.')] });
+      if (!msg.member.voice.channelId) return msg.channel.send({ embeds: [CreateEmbed('warn', '⛔ | you must join voice channel to do this.')] });
+      if (msg.member.voice.channelId !== GuildPlayers.voiceChannel) return msg.channel.send({ embeds: [CreateEmbed('warn', '⛔ | you must join voice channel same as me to do this.')] });
       GuildPlayers.setQueueRepeat(!GuildPlayers.queueRepeat);
       return msg.channel.send({ embeds: [CreateEmbed('info', `👌 | ${GuildPlayers.queueRepeat ? 'Enabled loop' : 'Disabled loop'}`)] });
     } catch (e) {
