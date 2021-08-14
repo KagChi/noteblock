516c9a9a09c96a8db6c8bb570d06c23652add3f1
カグチ <59391215+KagChi@users.noreply.github.com>
2021-08-14 15:06:40 +0700

    chore: add more slash commands

--- src/Commands/Music/StopCommand.js
+++ src/Commands/Music/StopCommand.js
@@ -26,4 +26,22 @@ module.exports = class StopCommand extends Command {
       return msg.channel.send({ embeds: [CreateEmbed('warn', '⛔ | An error occured')] });
     }
   }
+
+  /**
+   *
+   * @param {import('discord.js').CommandInteraction} interaction
+   */
+  async executeSlash(interaction) {
+    try {
+      const GuildPlayers = this.client.erela.players.get(interaction.guild.id);
+      if (!GuildPlayers) return interaction.editReply({ embeds: [CreateEmbed('info', '⛔ | There no music playing in this guild')] });
+      if (!interaction.member.voice.channelId) return interaction.editReply({ embeds: [CreateEmbed('warn', '⛔ | you must join voice channel to do this.')] });
+      if (interaction.member.voice.channelId !== GuildPlayers.voiceChannel) return interaction.editReply({ embeds: [CreateEmbed('warn', '⛔ | you must join voice channel same as me to do this.')] });
+      GuildPlayers.destroy();
+      return interaction.editReply({ embeds: [CreateEmbed('info', '👌 | Stopped guild queue')] });
+    } catch (e) {
+      this.client.logger.error(e.message);
+      return interaction.editReply({ embeds: [CreateEmbed('warn', '⛔ | An error occured')] });
+    }
+  }
 };
